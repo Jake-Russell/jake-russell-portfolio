c5cee81c0afc092fca0c626489e4dd7c0091714a
Jake Russell <jake.russell@valtech.com>
2023-11-10 16:53:06 +0000

    Added form template

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import aboutImage from "../public/assets/star_trails_great_coxwell.jpg";
 import React from "react";
 
 const About = () => {
@@ -6,7 +7,9 @@ const About = () => {
     <div className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
-          <p className="uppercase text-xl tracking-widest text-[#5651ef]">About</p>
+          <p className="uppercase text-xl tracking-widest text-[#5651ef]">
+            About
+          </p>
           <h2 className="py-4">Who I Am</h2>
           <p className="py-2 text-gray-600">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
@@ -20,15 +23,12 @@ const About = () => {
             culpa accusantium quas ut inventore tenetur sapiente at, sed officia
             assumenda voluptates veritatis?
           </p>
-          <p className="py-2 text-gray-600 underline cursor-pointer">Check out some of my latest projects</p>
+          <p className="py-2 text-gray-600 underline cursor-pointer">
+            Check out some of my latest projects
+          </p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <Image className="rounded-xl"
-            src="/assets/star_trails_great_coxwell.jpg"
-            alt="Logo"
-            width={6000}
-            height={4000}
-          />
+          <Image className="rounded-xl" src={aboutImage} alt="Logo" />
         </div>
       </div>
     </div>
