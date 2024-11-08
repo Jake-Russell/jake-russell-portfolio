0fe9441376bff601718bf7fce3a9c093c1fb2049
Jake <jake@megster.net>
2024-11-08 15:08:40 +0000

    Transition project item styles on hover

--- components/Projects/subcomponents/ProjectItem/index.tsx
+++ components/Projects/subcomponents/ProjectItem/index.tsx
@@ -12,13 +12,13 @@ const ProjectItem: React.FC<ProjectItemProps> = ({
   projectUrl,
 }) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-[#5651e5] transition duration-500">
       <Image
         src={backgroundImage}
         alt=""
-        className="rounded-xl group-hover:opacity-10 object-cover h-full"
+        className="transition duration-500 rounded-xl group-hover:brightness-50 object-cover h-full"
       />
-      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+      <div className="opacity-0 group-hover:opacity-100 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] transition duration-500">
         <h3
           className={`text-2xl text-white tracking-wider text-center ${
             !subtitle && 'pb-4'
